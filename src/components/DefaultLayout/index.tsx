a9b8e878d9e0aa57380e5606a7407904bbcb406c
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-09 13:40:58 -0300

    styling sidebar itens

--- src/components/DefaultLayout/index.tsx
+++ src/components/DefaultLayout/index.tsx
@@ -1,6 +1,13 @@
 import { styled } from '@/styles/stitches.config';
 import { useRouter } from 'next/router';
-import { Bell, Gear, House, List, UserCircle } from 'phosphor-react';
+import {
+  Bell,
+  Gear,
+  House,
+  List,
+  SketchLogo,
+  UserCircle,
+} from 'phosphor-react';
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
 
@@ -18,27 +25,33 @@ const DefaultLayout = ({ children }: DefaultLayout) => {
       </NavBar>
       <Layout>
         <SideBar>
-          <SideBarItem href={'/'} isActive={router.pathname === '/'}>
-            <House size={28} />
-            <span>Dashboard</span>
-          </SideBarItem>
+          <SideBarContent>
+            <SideBarItem href={'/'} isActive={router.pathname === '/'}>
+              <House size={28} />
+              <span>Dashboard</span>
+            </SideBarItem>
 
-          <SideBarItem
-            href={'/settings'}
-            isActive={router.pathname === '/settings'}
-          >
-            <Gear size={28} />
-            <span>Settings</span>
-          </SideBarItem>
+            <SideBarItem
+              href={'/settings'}
+              isActive={router.pathname === '/settings'}
+            >
+              <Gear size={28} />
+              <span>Settings</span>
+            </SideBarItem>
 
-          <SideBarItem
-            href={'/'}
-            disabled
-            isActive={router.pathname === '/Profile'}
-          >
-            <UserCircle size={28} />
-            <span>Profile</span>
-          </SideBarItem>
+            <SideBarItem
+              href={'/'}
+              disabled
+              isActive={router.pathname === '/Profile'}
+            >
+              <UserCircle size={28} />
+              <span>Profile</span>
+            </SideBarItem>
+          </SideBarContent>
+          <Button>
+            Upgrade PRO
+            <SketchLogo size={32} />
+          </Button>
         </SideBar>
         <Content>{children}</Content>
       </Layout>
@@ -72,6 +85,7 @@ export const SideBar = styled('div', {
   height: 'calc(100vh - 57px)',
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'space-between',
   gap: '.5rem',
 });
 
@@ -104,6 +118,26 @@ export const SideBarItem = styled(Link, {
     fontWeight: '500',
     fontSize: '1.2rem',
   },
+});
+
+export const SideBarContent = styled('div', {});
+
+export const Button = styled('button', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '1rem',
+  cursor: 'pointer',
+  width: '100%',
+  padding: '.8rem',
+  borderRadius: '6px',
+  textDecoration: 'none',
+  color: 'white',
+  fontWeight: '500',
+  fontSize: '1.2rem',
+  border: 'none',
+  background:
+    'linear-gradient(90deg, rgba(250,0,255,1) 0%, rgba(0,108,255,1) 100%)',
 });
 
 export const Content = styled('main', {
